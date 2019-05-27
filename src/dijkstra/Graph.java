333815f6fd81b7efac3f08594a535ca03b94c712
Max Stephan <max.stephan@mni.thm.de>
2019-05-27 18:02:38 +0200

    added basic implementation to Graph class

--- src/dijkstra/Graph.java
+++ src/dijkstra/Graph.java
@@ -1,35 +1,45 @@
 package dijkstra;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Graph implements IGraph {
-    @Override
-    public void addNode(Node n) {
+    private Set<Node> nodes;
+    private Set<Edge> edges;
 
+    public Graph() {
+        nodes = new HashSet<>();
+        edges = new HashSet<>();
     }
 
     @Override
     public Set<Node> getNodes() {
-        return null;
+        return nodes;
     }
 
     @Override
     public Set<Edge> getEdges() {
-        return null;
+        return edges;
+    }
+
+    @Override
+    public void addNode(Node n) {
+        nodes.add(n);
     }
 
     @Override
     public void link(Node a, Node b) {
-
+        edges.add(new Edge(a, b));
     }
 
     @Override
     public Path findShortestPath(Node start, Node end) {
+        //TODO
         return null;
     }
 
     @Override
     public void fillAdjMatrix() {
-
+        //TODO
     }
 }
