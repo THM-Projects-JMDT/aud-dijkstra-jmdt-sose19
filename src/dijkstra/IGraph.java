d9504c3cd253825f1dd24a144e71239032f3157f
David <david.martschenko@web.de>
2019-05-27 14:03:12 +0200

    added Graph-Interface

---
+++ src/dijkstra/IGraph.java
@@ -0,0 +1,12 @@
+package dijkstra;
+
+import java.util.List;
+
+public interface IGraph {
+    void addNode(Node n);
+    Set<Node> getNodes();
+    Set<Edge> getEdges();
+    void link(Node a, Node b);
+    Path findShortestPath(Node start, Node end);
+    void fillAdjMatrix();
+}
