5e1304fc170e1d656be93615d4401152c4f18f54
Jannik-PC <jannik.lapp@mni.thm.de>
2019-05-27 20:46:40 +0200

    added test field

--- src/graphen/TestUI.java
+++ src/graphen/TestUI.java
@@ -3,11 +3,19 @@ package graphen;
 public class TestUI {
     public static void main(String[] args) {
         Graph g1 = new Graph();
-        g1.addNode(5,8);
-        g1.addNode(0,0);
-        g1.addNode(6,1);
-        g1.addNode(14,8);
-        g1.addNode(7,2);
+        Node n0= g1.addNode(0,0);
+        Node n1= g1.addNode(2,2);
+        Node n2= g1.addNode(1,2);
+        Node n3= g1.addNode(0,2);
+        Node n4= g1.addNode(2,0);
+        Node n5= g1.addNode(3,3);
+        g1.link(n0,n4);
+        g1.link(n4,n2);
+        g1.link(n0,n0);
+        g1.link(n2,n3);
+        g1.link(n3,n1);
+        g1.link(n1,n5);
+        g1.link(n2,n5);
     }
 
 
