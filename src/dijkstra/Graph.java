6cbc1df4deb492f316f889430aae39dbced30feb
Max Stephan <max.stephan@mni.thm.de>
2019-05-27 17:51:44 +0200

    added Graph class

---
+++ src/dijkstra/Graph.java
@@ -0,0 +1,35 @@
+package dijkstra;
+
+import java.util.Set;
+
+public class Graph implements IGraph {
+    @Override
+    public void addNode(Node n) {
+
+    }
+
+    @Override
+    public Set<Node> getNodes() {
+        return null;
+    }
+
+    @Override
+    public Set<Edge> getEdges() {
+        return null;
+    }
+
+    @Override
+    public void link(Node a, Node b) {
+
+    }
+
+    @Override
+    public Path findShortestPath(Node start, Node end) {
+        return null;
+    }
+
+    @Override
+    public void fillAdjMatrix() {
+
+    }
+}
