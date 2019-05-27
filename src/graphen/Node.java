86d1fc5c7aac503da0b516ed0753d34ad2e37612
Jannik-PC <jannik.lapp@mni.thm.de>
2019-05-27 20:50:43 +0200

    added equals and hash methode to Note

--- src/graphen/Node.java
+++ src/graphen/Node.java
@@ -1,5 +1,7 @@
 package graphen;
 
+import java.util.Objects;
+
 public class Node {
     private int x;
     private int y;
@@ -15,5 +17,19 @@ public class Node {
     public Node(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return x == node.x &&
+                y == node.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
