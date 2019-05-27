e8acfaebf85b0d4fe06fdd8a4098164406c4fb81
David <david.martschenko@web.de>
2019-05-27 14:55:45 +0200

    added get/set-methods for Node

--- src/dijkstra/Node.java
+++ src/dijkstra/Node.java
@@ -4,6 +4,14 @@ public class Node {
     private int x;
     private int y;
 
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
     public Node(int x, int y) {
         this.x = x;
         this.y = y;
