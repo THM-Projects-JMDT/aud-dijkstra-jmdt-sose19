865a984f5f976c9c4fd6859c0b15c31876717325
tplk70 <timon.pellekoorne@mni.thm.de>
2019-05-28 12:47:34 +0200

    Added Circles to Design

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,13 +2,17 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOError;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application {
 
@@ -24,10 +28,22 @@ public class Main extends Application {
         try{
             FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml"));
             Parent root = loader.load();
-            primaryStage.setTitle("Hello World");
-            primaryStage.setScene(new Scene(root, 1080, 720));
-            primaryStage.setResizable(false);
+
+            Group group = new Group();
+            Scene scene = new Scene(group, 600,600);
+            List<Circle> circles = new ArrayList<>();
+
+            circles.add(new Circle(100,100,10));
+            circles.add(new Circle(300,150,10));
+            circles.add(new Circle(50,200,10));
+            circles.add(new Circle(400,500,10));
+
+            for (Circle circle :circles) {
+                group.getChildren().add(circle);
+            }
+            primaryStage.setScene(scene);
             primaryStage.show();
+
 
             Controller controller = loader.getController();
             controller.setMain(this);
