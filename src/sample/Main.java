0c3b1155b5c49b32372d2b5266d4cf885b7a7edb
Lisi <lisa.soboth@mni.thm.de>
2019-05-28 12:51:10 +0200

    Added Lines to Graph-Design

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 import java.io.IOError;
@@ -32,18 +33,30 @@ public class Main extends Application {
             Group group = new Group();
             Scene scene = new Scene(group, 600,600);
             List<Circle> circles = new ArrayList<>();
+            List<Line> lines = new ArrayList<>();
 
             circles.add(new Circle(100,100,10));
             circles.add(new Circle(300,150,10));
             circles.add(new Circle(50,200,10));
             circles.add(new Circle(400,500,10));
 
+            lines.add(new Line(100,100,300,150));
+            lines.add(new Line(100,100,50,200));
+            lines.add(new Line(300,150,400,500));
+            lines.add(new Line(50,200,400,500));
+            lines.add(new Line(300,150,50,200));
+
             for (Circle circle :circles) {
                 group.getChildren().add(circle);
             }
+
+            for (Line l:lines) {
+                l.setStrokeWidth(2);
+                group.getChildren().add(l);
+            }
+
             primaryStage.setScene(scene);
             primaryStage.show();
-
 
             Controller controller = loader.getController();
             controller.setMain(this);
