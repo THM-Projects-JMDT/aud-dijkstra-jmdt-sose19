e38553a0fe259682557255c8bfbda2ccf8e3d11b
Max Stephan <max.stephan@mni.thm.de>
2019-05-28 11:48:11 +0200

    added contains method

--- src/dijkstra/Edge.java
+++ src/dijkstra/Edge.java
@@ -14,4 +14,10 @@ public class Edge {
     private void calculateDistance() {
         this.distance = Math.sqrt(Math.pow(b.getX() - a.getX(), 2) + Math.pow(b.getY() - a.getY(), 2));
     }
+
+    private boolean contains(Node n) {
+        if(a.equals(n)) return true;
+        if(b.equals(n)) return true;
+        return false;
+    }
 }
