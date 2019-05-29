55e639ebcf162599932f784528c0702dfb849384
tplk70 <timon.pellekoorne@mni.thm.de>
2019-05-29 12:34:17 +0200

    created Graph in JavaFX

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -24,6 +24,11 @@ import java.util.List;
 public class Main extends Application {
 
     private Stage primaryStage;
+    private List<Circle> circles = new ArrayList<>();
+    private List<Line> lines = new ArrayList<>();
+    private List<Label> labeleCircles = new ArrayList<>();
+    private List<Label> labelLines = new ArrayList<>();
+    private Graph graph = new Graph();
 
     @Override
     public void start(Stage primaryStage){
@@ -38,35 +43,39 @@ public class Main extends Application {
             Group group = new Group();
             Scene scene = new Scene(group, 600,600);
 
-            List<Circle> circles = new ArrayList<>();
-            List<Line> lines = new ArrayList<>();
 
-            Graph graph = new Graph();
 
             //Knoten erzeugen
             Node node1 = new Node(100,100);
-            Node node2 = new Node(300,300);
-            Node node3 = new Node(405, 300);
+            Node node2 = new Node(300,100);
+            Node node3 = new Node(400, 100);
+            Node node4 = new Node(200,200);
+            Node node5 = new Node(400, 200);
+            Node node6 = new Node(300,400);
+            Node node7 = new Node(500,300);
+            Node node8 = new Node(100,500);
+            Node node9 = new Node(500,500);
 
-            Circle circle1 = new Circle(node1.getX(),node1.getY(),5);
-            Label labelCircle = new Label(String.valueOf(node1.getLabel()));
-            circles.add(circle1);
-            graph.addNode(node1);
+            createCircle(node1);
+            createCircle(node2);
+            createCircle(node3);
+            createCircle(node4);
+            createCircle(node5);
+            createCircle(node6);
+            createCircle(node7);
+            createCircle(node8);
+            createCircle(node9);
 
-            circles.add(new Circle(node2.getX(),node2.getY(),5));
-            graph.addNode(node2);
-            circles.add(new Circle(node3.getX(),node3.getY(),5));
-            graph.addNode(node3);
+            createLine(node1, node4);
+            createLine(node2, node4);
+            createLine(node2, node3);
+            createLine(node4, node6);
+            createLine(node6, node7);
+            createLine(node7, node9);
+            createLine(node6, node9);
+            createLine(node6, node8);
 
-            Edge e = graph.link(node1,node2);
 
-            Line line = new Line(node1.getX(),node1.getY(),node2.getX(),node2.getY());
-            Label labelLine = new Label(Math.round(e.getDistance()) + "");
-
-            drawLine(line, labelLine);
-            drawCircle(circle1, labelCircle);
-
-            lines.add(line);
 
             for (Circle circle :circles) {
                 group.getChildren().add(circle);
@@ -76,9 +85,12 @@ public class Main extends Application {
                 l.setStrokeWidth(2);
                 group.getChildren().add(l);
             }
-
-            group.getChildren().add(labelCircle);
-            group.getChildren().add(labelLine);
+            for (Label label: labeleCircles) {
+                group.getChildren().add(label);
+            }
+            for (Label label: labelLines) {
+                group.getChildren().add(label);
+            }
 
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -91,13 +103,31 @@ public class Main extends Application {
         }
     }
 
-    private void drawCircle(Circle circle, Label label){
+    private void createCircle(Node node) {
+        Circle circle1 = new Circle(node.getX(),node.getY(),5);
+        Label labelCircle = new Label(String.valueOf(node.getLabel()));
+        circles.add(circle1);
+        graph.addNode(node);
+        labeleCircles.add(labelCircle);
+        attachLabelToCircle(circle1,labelCircle);
+    }
+
+    private void createLine(Node node1, Node node2) {
+        Edge e = graph.link(node1,node2);
+        Line line = new Line(node1.getX(),node1.getY(),node2.getX(),node2.getY());
+        Label labelLine = new Label(Math.round(e.getDistance()) + "");
+        lines.add(line);
+        labelLines.add(labelLine);
+        attachLabelToLine(line,labelLine);
+    }
+
+    private void attachLabelToCircle(Circle circle, Label label){
         label.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
         label.layoutXProperty().bind((circle.centerXProperty().add(10)));
         label.layoutYProperty().bind((circle.centerYProperty().add(-10)));
     }
 
-    private void drawLine(Line line, Label label) {
+    private void attachLabelToLine(Line line, Label label) {
         label.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
         label.layoutXProperty().bind((line.endXProperty().add(line.startXProperty())).divide(2));
         label.layoutYProperty().bind((line.endYProperty().add(line.startYProperty())).divide(2));
