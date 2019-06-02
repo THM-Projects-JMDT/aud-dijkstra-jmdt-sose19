d80ce1d9a58a2d69e7b163a2caadc0eb411666f9
Max Stephan <max.stephan@mni.thm.de>
2019-06-02 16:59:36 +0200

    Buttons moved out of main class

---
+++ src/visualization/ButtonController.java
@@ -0,0 +1,78 @@
+package visualization;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import sample.Main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ButtonController implements Initializable {
+    private Looping looping;
+
+    @FXML
+    private Button btnGo;
+
+    @FXML
+    private Button btnLoop;
+
+    @FXML
+    private Button btnRandom;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    void clear(ActionEvent event) {
+        App.getGraphController().clear();
+    }
+
+    @FXML
+    void go(ActionEvent event) {
+        App.getGraphController().next();
+    }
+
+    @FXML
+    void loop(ActionEvent event) {
+        if(looping.running) {
+            stopLoop();
+        } else {
+            startLoop();
+        }
+    }
+
+    @FXML
+    void random(ActionEvent event) {
+        App.getGraphController().random();
+    }
+
+    public void stopLoop() {
+        btnLoop.setText("AUTO-START");
+        looping.running = false;
+    }
+
+    public void startLoop() {
+        looping = new Looping();
+        looping.start();
+        btnLoop.setText("STOPP");
+    }
+
+    public void enableButtons() {
+        btnGo.setDisable(false);
+        btnLoop.setDisable(false);
+    }
+
+    public void disableButtons() {
+        btnGo.setDisable(true);
+        btnLoop.setDisable(true);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        App.setButtonController(this);
+        looping = new Looping();
+        looping.running = false;
+    }
+}
