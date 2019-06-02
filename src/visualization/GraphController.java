179a9768618d9eb8292ee53746ca0c70213afcbb
Max Stephan <max.stephan@mni.thm.de>
2019-06-02 17:04:30 +0200

    remove all graph processing methods from the main class

---
+++ src/visualization/GraphController.java
@@ -0,0 +1,106 @@
+package visualization;
+
+import dijkstra.Edge;
+import dijkstra.Generator;
+import dijkstra.Node;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GraphController implements Initializable {
+    private DrawGraph drawGraph;
+    private GraphState graphState;
+    private boolean switcher;
+    private boolean first;
+
+    @FXML
+    private Pane centerGroup;
+
+    @FXML
+    private Label error;
+
+    @FXML
+    void setNode(MouseEvent event) {
+        if(graphState.connected()) {
+            if(!(graphState.isFrom(graphState.getStart()) || graphState.isFrom(graphState.getEnd()))) {
+                drawGraph.resetStart();
+            }
+            graphState.setFrom(null);
+            graphState.changeConected(false);
+            return;
+        }
+        if(graphState.finished()) return;
+        int x = (int) Math.round(event.getX());
+        int y = (int) Math.round(event.getY());
+        Node n = new Node(x,y);
+        drawGraph.createCircle(n);
+        drawGraph.addCircle(n);
+    }
+
+    public void init() {
+        drawGraph = new DrawGraph(centerGroup, error);
+        graphState = drawGraph.getGraphState();
+        first = true;
+        switcher = true;
+    }
+
+    public void clear() {
+        Node.resetCounter();
+        App.getButtonController().disableButtons();
+        centerGroup.getChildren().clear();
+        centerGroup.getChildren().removeAll();
+        init();
+        App.getButtonController().stopLoop();
+    }
+
+    public void random() {
+        clear();
+        App.getButtonController().enableButtons();
+        Generator g = new Generator();
+        graphState.setGraph(g.generateGraph());
+        for (Node n : graphState.getGraph().getNodes()) {
+            drawGraph.createCircle(n);
+        }
+        for (Edge e : graphState.getGraph().getEdges()) {
+            drawGraph.createLine(e.getA(), e.getB());
+        }
+        drawGraph.newGraph(g.node1, g.node2);
+    }
+
+    public void initializePlay() {
+        graphState.findShortestPath();
+        graphState.iteratorInit();
+        graphState.changeEndselected(true);
+        graphState.changeFinished(true);
+        App.getButtonController().enableButtons();
+    }
+
+    public void next() {
+        if (graphState.finished()) {
+            drawGraph.resetNoNeighbours();
+            if (graphState.getEdgeIt().hasNext() && switcher && !first) {
+                drawGraph.markLine(graphState.getEdgeIt().next(), Color.ORANGERED);
+            } else if (graphState.getNodeIt().hasNext()) {
+                drawGraph.updateLabels(graphState.getNodeIt().next());
+                first = false;
+            } else {
+                drawGraph.markOptimalPath();
+                switcher = false;
+                first = true;
+            }
+            switcher = !switcher;
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        App.setGraphController(this);
+        clear();
+    }
+}
