9ace75a67b3b8a6f2674efe4473ea60d27757133
Max Stephan <max.stephan@mni.thm.de>
2019-06-03 10:54:20 +0200

    function of commit "added primaryScreenBounds, that application now works on every screen" added again

--- src/visualization/App.java
+++ src/visualization/App.java
@@ -3,28 +3,26 @@ package visualization;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @FXML
     private static ButtonController buttonController;
 
+    @FXML
+    private static GraphController graphController;
+
     public static ButtonController getButtonController() {
         return buttonController;
     }
 
-    public static GraphController getGraphController() {
-        return graphController;
-    }
+    public static GraphController getGraphController() { return graphController; }
 
-    @FXML
-    private static GraphController graphController;
-
-    public static void setButtonController(ButtonController buttonController) {
-        App.buttonController = buttonController;
-    }
+    public static void setButtonController(ButtonController buttonController) { App.buttonController = buttonController; }
 
     public static void setGraphController(GraphController graphController) {
         App.graphController = graphController;
@@ -34,7 +32,9 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("resources/scene/dijkstra.fxml"));
         primaryStage.setTitle("Dijkstra");
-        primaryStage.setScene(new Scene(root, 1920,720));
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        Scene scene = new Scene(root, primaryScreenBounds.getWidth(),primaryScreenBounds.getHeight());
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
